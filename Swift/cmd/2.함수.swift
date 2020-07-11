bedf9afe4f76fc1088e2b7315975cc25386c8494
argon1025 <argon1025@gmail.com>
2020-07-11 23:19:22 +0900

    swift 함수_2

--- Swift/cmd/2.함수.swift
+++ Swift/cmd/2.함수.swift
@@ -23,3 +23,31 @@ import Foundation
 func sum(a: Int, b: Int) -> Int {
     return a + b
 }
+
+
+
+/*매개변수 기본값 할당*/
+func greeting(name: String, age: String = "15") {
+}
+
+
+
+/*전달인자 레이블(Argument Label)*/
+func greeting(to friend: String, from me: String) {
+    print("Hello \(friend)! I'm \(me)")
+}
+//greeting(to: "hana", from: "yagom")
+
+
+
+/*가변 매개변수*/
+func sayHelloToFriends(me: String, friends: String...) -> String {
+    return "Hello \(friends)! I'm \(me)!"
+}
+//print(sayHelloToFriends(me: "yagom", friends: "hana", "eric", "wing"))
+// Hello ["hana", "eric", "wing"]! I'm yagom!
+
+
+
+/*데이터 타입으로써의 함수*/
+var someFunction: (String, String) -> Void = greeting(name:age:)
