286f33fc14bbd3f56ac4c7dfdc9d6e6d3bbc909f
argon1025 <argon1025@gmail.com>
2020-07-11 23:15:26 +0900

    swift 함수

---
+++ Swift/cmd/2.함수.swift
@@ -0,0 +1,25 @@
+//
+//  2.함수.swift
+//  Swift_Tutorial
+//
+//  Created by 이성록 on 2020/07/11.
+//  Copyright © 2020 Leesungrok. All rights reserved.
+//
+
+import Foundation
+
+
+/*
+ 함수
+ func 함수명(매개변수이름: 매개변수타입)-> 반환타입Void {
+ 
+ 
+ 함수 호출
+ sum(a:3, b:5)
+ voidfunc()
+ hello(name:"test")
+ }
+ */
+func sum(a: Int, b: Int) -> Int {
+    return a + b
+}
