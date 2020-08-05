ed6c22d1bddef4df15587ef696d9425dfb8c6693
argon1025 <argon1025@gmail.com>
2020-08-05 17:39:23 +0900

    create project

---
+++ Swift/SwiftUI/SimpleMusicPlayer/SimpleMusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMusicPlayer
+//
+//  Created by 이성록 on 2020/08/05.
+//  Copyright © 2020 Leesungrok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
