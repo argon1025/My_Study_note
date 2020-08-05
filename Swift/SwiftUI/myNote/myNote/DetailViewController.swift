46bc2aa42f06b43673f9fb6aab11129cc258e2b2
argon1025 <argon1025@gmail.com>
2020-08-06 00:44:34 +0900

    Memo detailView ADD

---
+++ Swift/SwiftUI/myNote/myNote/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  myNote
+//
+//  Created by 이성록 on 2020/08/06.
+//  Copyright © 2020 Leesungrok. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
