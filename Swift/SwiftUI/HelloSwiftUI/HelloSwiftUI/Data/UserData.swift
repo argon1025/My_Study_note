60d8b4b088890cb0b3937d1b9c81880649a45629
argon1025 <argon1025@gmail.com>
2020-08-07 19:56:27 +0900

    apple swiftui tutorials_5

---
+++ Swift/SwiftUI/HelloSwiftUI/HelloSwiftUI/Data/UserData.swift
@@ -0,0 +1,18 @@
+//
+//  UserData.swift
+//  HelloSwiftUI
+//
+//  Created by 이성록 on 2020/08/07.
+//  Copyright © 2020 Leesungrok. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+
+final class UserData: ObservableObject  {
+    
+    @Published var showFavoritesOnly = false
+    //@Published var landmarks = landmarkData
+
+}
--- Swift/SwiftUI/HelloSwiftUI/HelloSwiftUI/LandmarkList.swift
+++ Swift/SwiftUI/HelloSwiftUI/HelloSwiftUI/LandmarkList.swift
@@ -9,11 +9,19 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State var showFavoritesOnly = false
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                NavigationLink(destination: ContentView(landmark: landmark)){
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(landmarkData) { landmark in
+                    if !self.showFavoritesOnly || landmark.isFavorite ?? false {
+                    NavigationLink(destination: ContentView(landmark: landmark)){
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
                 }
             }
             .navigationBarTitle(Text("관광명소"))
@@ -23,7 +31,8 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE", "iPhone XS Max","iPad Pro (10.5-inch)"], id: \.self) { deviceName in
+        //, "iPhone XS Max","iPad Pro (10.5-inch)"
+        ForEach(["iPhone SE"], id: \.self) { deviceName in
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
