a892d357f0b5b73f881642bfca11bb0083285dd7
argon1025 <argon1025@gmail.com>
2021-01-13 17:18:51 +0900

    NodeJS Create Server study 2 file read

--- Javascript/NodeJS/helloworld.js
+++ Javascript/NodeJS/helloworld.js
@@ -1,20 +1,15 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 // ------------------------------------
 
 
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
-    }
-    if(request.url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+
+    console.log(_url);//전체 URL
+    console.log(queryData); //쿼리 스트링
  
 });
-app.listen(3000);
+app.listen(80);
