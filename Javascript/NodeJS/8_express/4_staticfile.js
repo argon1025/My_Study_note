944ad528387f9c6b6d3be608cbe3f8bf0148b339
argon1025 <argon1025@gmail.com>
2021-01-13 21:12:38 +0900

    NodeJS Create Server study 8 express_3 staticfile

---
+++ Javascript/NodeJS/8_express/4_staticfile.js
@@ -0,0 +1,19 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+const port = 80
+
+var Loger = function(req, res, call) { //인자는 req, res callback 함수로 정해져 있음
+    console.log('Log');
+    call();
+}
+app.use(Loger);
+app.use(express.static('res')); //http://127.0.0.1/photo.webp
+//routing
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
